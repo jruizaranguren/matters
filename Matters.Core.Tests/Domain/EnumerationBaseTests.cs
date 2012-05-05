587230fcf1f7f2b153fbed65192a1f52d974bdaa
Javier Ruiz Aranguren <Mireia@BERTIZ223B.(none)>
2012-05-05 19:01:57 +0200

    Added EnumerationBase and CompareObjects. Assemblies signed

---
+++ Matters.Core.Tests/Domain/EnumerationBaseTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Matters.Core.Domain;
+
+namespace Matters.Core.Tests.Domain
+{
+    [TestClass]
+    public class EnumerationBaseTests
+    {
+        [TestMethod]
+        public void RawEnumerationClassIsCorrectlyInitialized()
+        {
+            var expectedNumberItems = 3;
+            var actualNumberItems = RawEnumerationClass.GetValues().Count();
+            Assert.AreEqual(expectedNumberItems, actualNumberItems);
+        }
+
+        [TestMethod]
+        public void CustomTypeEnumerationClassIsCorrectlyInitialized()
+        {
+            var expectedNumberItems = 3;
+            var expectedVat = 0.07M;
+            var actualNumberItems = CustomTypeEnumerationClass.GetValues().Count();
+            Assert.AreEqual(expectedNumberItems, actualNumberItems);
+            Assert.AreEqual(expectedVat, CustomTypeEnumerationClass.FirstValue.Value.Vat);
+        }
+
+        [TestMethod]
+        public void EnumerationBaseEqualOperatorWorks()
+        {
+            var consumer = new RawEnumerationConsumer();
+            Assert.AreEqual(RawEnumerationClass.FirstValue, consumer.RawInstance);
+            Assert.AreNotEqual(RawEnumerationClass.SecondValue, consumer.RawInstance);
+        }
+    }
+
+    /// <summary>
+    /// Example of a class with static values
+    /// </summary>
+    public sealed class RawEnumerationClass : EnumerationBase<RawEnumerationClass, Guid, string>
+    {
+        static RawEnumerationClass()
+        {
+            FirstValue = Register(new Guid("5CA84018-AB60-4D7C-808B-26EB400D6F25"), "First Value");
+            SecondValue = Register(new Guid("6DFED3E6-AD72-4605-975C-72A5A2880F6C"), "Second Value");
+            ThirdValue = Register(new Guid("CFD1C6FB-5F75-4B35-A5AC-26F0D019A9C7"), "Third Value");
+        }
+
+        public static readonly RawEnumerationClass FirstValue;
+        public static readonly RawEnumerationClass SecondValue;
+        public static readonly RawEnumerationClass ThirdValue;
+
+    }
+
+
+
+    /// <summary>
+    /// Example with a Custom class as TValue
+    /// </summary>
+    public sealed class CustomTypeEnumerationClass : EnumerationBase<CustomTypeEnumerationClass, Guid, CustomType>
+    {
+        static CustomTypeEnumerationClass()
+        {
+            FirstValue = Register(new Guid("5CA84018-AB60-4D7C-808B-26EB400D6F25"), new CustomType("First Value", 0.07M));
+            SecondValue = Register(new Guid("6DFED3E6-AD72-4605-975C-72A5A2880F6C"), new CustomType("Second Value", 0.19M));
+            ThirdValue = Register(new Guid("CFD1C6FB-5F75-4B35-A5AC-26F0D019A9C7"), new CustomType("Third Value", 0));
+        }
+
+        public static readonly CustomTypeEnumerationClass FirstValue;
+        public static readonly CustomTypeEnumerationClass SecondValue;
+        public static readonly CustomTypeEnumerationClass ThirdValue;
+
+        public bool IsFirstValue()
+        {
+            return ReferenceEquals(this, FirstValue);
+        }
+    }
+
+    public sealed class CustomType
+    {
+        public string Description { get; private set; }
+        public decimal Vat { get; private set; }
+
+        public CustomType(string description, decimal vat)
+        {
+            Description = description;
+            Vat = vat;
+        }
+    }
+
+    public sealed class RawEnumerationConsumer
+    {
+        private RawEnumerationClass _rawInstance;
+
+        public RawEnumerationClass RawInstance
+        {
+            get { return _rawInstance; }
+        }
+
+        public RawEnumerationConsumer()
+        {
+            _rawInstance = RawEnumerationClass.FirstValue;
+        }
+
+    }
+
+}
---
+++ Matters.Core/Domain/EnumerationBase.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Matters.Core.Domain
+{
+    public abstract class EnumerationBase<T, TId, TValue> where T : EnumerationBase<T, TId, TValue>, new()
+    {
+        public TId Code { get; private set; }
+        public TValue Value { get; private set; }
+        public static T Default { get; private set; }
+
+        private static readonly Dictionary<TId, T> Main = new Dictionary<TId, T>();
+
+        static EnumerationBase()
+        {
+            new T();
+        }
+
+        protected static T Register(TId id, TValue value)
+        {
+            var t = new T { Code = id, Value = value };
+            Main.Add(id, t);
+            return t;
+        }
+
+        public static IEnumerable<T> GetValues()
+        {
+            return Main.Values.ToList();
+        }
+
+        protected static void DefaultTo(T defaultvalue)
+        {
+            Default = defaultvalue;
+        }
+
+        public static bool operator ==(EnumerationBase<T, TId, TValue> a, T b)
+        {
+            if ((object)a == null && (object)b == null) return true;
+            if ((object)a == null || (object)b == null) return false;
+            return a.Code.Equals(b.Code);
+        }
+
+        public static bool operator !=(EnumerationBase<T, TId, TValue> a, T b)
+        {
+            return !(a == b);
+        }
+
+        public bool Equals(EnumerationBase<T, TId, TValue> other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(other.Code, Code);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof(EnumerationBase<T, TId, TValue>)) return false;
+            return Equals((EnumerationBase<T, TId, TValue>)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Code.GetHashCode();
+        }
+    }
+}
+
+
+
+
